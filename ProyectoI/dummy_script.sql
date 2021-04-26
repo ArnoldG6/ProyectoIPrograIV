a18ed66a5b19100752b3a6a29c0214f003433370
arnoldgq612 <arnoldgq612@gmail.com>
2021-04-26 10:49:10 -0600

    modelo

--- ProyectoI/dummy_script.sql
+++ ProyectoI/dummy_script.sql
@@ -1,1 +1,7 @@
 SET GLOBAL time_zone = '-6:00';
+SELECT admin_id, username, email, pho_num, pass FROM administrators;
+SELECT DATABASE(); 
+USE universidad;
+select * from information_schema.schemata; 
+show databases;
+
